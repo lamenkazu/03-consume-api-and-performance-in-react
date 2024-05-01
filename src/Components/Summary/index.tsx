a2450e099e4a92ca99872cba9b019bcd626a9d6c
Lamenkazu <333.erick@gmail.com>
2024-05-01 09:39:36 -0300

    ✨ Formatação de valores Data e Dinheiro - Utils

--- src/Components/Summary/index.tsx
+++ src/Components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { Card, Container } from "./styles";
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 
 export const Summary = () => {
   const { transactions } = useContext(TransactionsContext);
@@ -33,7 +34,7 @@ export const Summary = () => {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </Card>
 
       <Card>
@@ -42,7 +43,7 @@ export const Summary = () => {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </Card>
 
       <Card $variant="green">
@@ -51,7 +52,7 @@ export const Summary = () => {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </Card>
     </Container>
   );
